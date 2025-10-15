8f72a1eaf27732c19579019c02a523cb73076716
Lugia11 <noahgunnarsson2011@hotmail.com>
2025-10-15 13:56:52 +0200

    Create pythonfile.py

---
+++ pythonfile.py
@@ -0,0 +1,14 @@
+#imports
+import os
+import random
+
+#random selects
+x = random.randint(1,3)
+
+if x == 1:
+  print("you win")
+elif x == 2:
+  print("You win")
+else:
+  os.open(Mina filer/AdolfHitlerBakery.txt)
+
